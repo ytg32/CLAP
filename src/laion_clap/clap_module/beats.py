246cc7f593eddefed4c2bc3fd986b581a4848466
Bora <boraa@ntnu.no>
2025-03-06 22:15:26 +0100

    Added: Self Contained BEATs. Fixed: Possible bug during feature extraction

--- src/laion_clap/clap_module/beats.py
+++ src/laion_clap/clap_module/beats.py
@@ -1,9 +1,9 @@
-from beans.beats.BEATs import BEATs, BEATsConfig
+from .beats_model.BEATs import BEATs, BEATsConfig
 import torch 
 import torch.nn as nn
 
-class BEATsClassifier(nn.Module):
-    def __init__(self, model_path, num_classes, multi_label=False):
+class BEATsClassifier(nn.Module): # Name does not make sense, it is more like encoder/backbone
+    def __init__(self, model_path):
         super().__init__()
         checkpoint = torch.load(model_path)
         cfg = BEATsConfig(checkpoint['cfg'])
@@ -11,11 +11,7 @@ class BEATsClassifier(nn.Module):
         self.model = BEATs(cfg)
         self.model.load_state_dict(checkpoint['model'])
 
-        self.model.finetuned_model = False
-        self.model.predictor_class = num_classes
-
-        self.model.predictor_dropout = nn.Dropout(cfg.predictor_dropout)
-        self.model.predictor = nn.Linear(cfg.encoder_embed_dim, num_classes)
+        self.sap = SelfAttentionPooling(496)
     
     def forward(self, x: torch.Tensor, mixup_lambda = None, infer_mode = False, device=None):
         sample = {}
@@ -27,11 +23,13 @@ class BEATsClassifier(nn.Module):
         else:
             mask = torch.zeros(x.shape).bool()
         
-        sample["embedding"] = self.model.extract_features(x, mask)[0]
+        #print(x.shape)
+        embedding = self.model.extract_features(x, mask)[0]
+        sample["embedding"] = torch.mean(embedding, dim = 1) 
         return sample
 
 
 
 def create_beats_model(audio_cfg, enable_fusion=False, fusion_type='None'):
-    return BEATsClassifier("/cluster/work/boraa/beans/model_weights/beats.pth", audio_cfg.class_num)
+    return BEATsClassifier("/cluster/work/boraa/beans/model_weights/beats.pth")
     
